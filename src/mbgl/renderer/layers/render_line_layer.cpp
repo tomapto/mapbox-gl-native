67a186608c4bc9499fb48d89b3dc6607cc71a301
Lauren Budorick <lauren@mapbox.com>
2017-06-14 15:32:56 -0700

    Statically cast concatenated properties to PossiblyEvaluated

--- src/mbgl/renderer/layers/render_line_layer.cpp
+++ src/mbgl/renderer/layers/render_line_layer.cpp
@@ -30,7 +30,7 @@ void RenderLineLayer::evaluate(const PropertyEvaluationParameters& parameters) {
     auto dashArrayParams = parameters;
     dashArrayParams.useIntegerZoom = true;
 
-    evaluated = RenderLinePaintProperties::PossiblyEvaluated(
+    evaluated = static_cast<RenderLinePaintProperties::PossiblyEvaluated>(
         unevaluated.evaluate(parameters).concat(extra.evaluate(dashArrayParams)));
 
     passes = (evaluated.get<style::LineOpacity>().constantOr(1.0) > 0
